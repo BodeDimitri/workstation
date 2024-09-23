84c5815ed2599c7bc489b7b153fa375805c6d99b
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2024-09-23 15:15:00 -0300

    Update ppline_docker_git.sh

--- ppline_docker_git.sh
+++ ppline_docker_git.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+######################################
+#Definir variaveis
+######################################
+
+#docker-compose Directory
 root_dir=""
-root_dir_back=""
-container_name_back=""
-root_dir_front=""
-container_name_front=""
+#Front
 frontend_dir=""
 frontend_repo=""
+#container_name_front=""
+#Back
 backend_dir=""
 backend_repo=""
+#container_name_back=""
 
+######################################
+#Função
+######################################
 
 check_update() {
     local dir=$1
@@ -21,18 +29,24 @@ check_update() {
     output=$(git pull "$repo_url" 2>&1)
     if echo "$output" | grep -q "Already up to date."; then
         status=1 
-        if  docker ps -a | awk '$2 ~ /$container_name_back/ {print $7}' | grep -q "Exited" || docker ps -a | awk '$2 ~ /container_name_front/ {print $7}' | grep -q "Exited" ; then 
-            echo "Repo atualizado e container não esta de pé"
+        if  docker ps -a | awk '$2 ~ /#/ {print $7}' | grep -q "Exited" || docker ps -a | awk '$2 ~ /# {print $7}' | grep -q "Exited" ; then 
+			echo "##########################################"
+            echo "Repo atualizado e container não esta de pé."
+			echo "##########################################"
             status=1
             cd "$root_dir"
             docker compose build
             docker-compose up -d
         else
+			echo "##########################################"
             echo "Repo atualizado e container já está de pé."
+			echo "##########################################"
         fi
     else
         status=0
+		echo "##################################################"
         echo "Novo pull recebido, o repositório foi atualizado."
+		echo "##################################################"
     fi
     return $status
 }
@@ -46,12 +60,12 @@ check_update "$frontend_dir" "$frontend_repo"
 resultFront=$?
 
 if [ "$resultBack" -eq 0 ] || [ "$resultFront" -eq 0 ]; then
-    echo "#########################################################################"
-    echo "Novo pull encontrado, fazendo o deploy automático e subindo novo container"
-    echo "#########################################################################"
+    echo "##########################################################################"
+    echo "Novo pull encontrado, fazendo o deploy automático e subindo novo container."
+    echo "##########################################################################"
 
-    docker ps -a | awk '$2 ~/$container_name_front/ {print $1}' | xargs docker stop | xargs docker rm
-    docker ps -a | awk '$2 ~/$container_name_back/ {print $1}' | xargs docker stop | xargs docker rm
+    docker ps -a | awk '$2 ~/#/ {print $1}' | xargs docker stop | xargs docker rm
+    docker ps -a | awk '$2 ~/#/ {print $1}' | xargs docker stop | xargs docker rm
 
     echo "#########################################################################"
     echo "Parando o container"
@@ -62,16 +76,16 @@ if [ "$resultBack" -eq 0 ] || [ "$resultFront" -eq 0 ]; then
     docker-compose up -d
 
     echo "#########################################################################"
-    echo "O container está subindo"
+    echo "O container está subindo."
     echo "#########################################################################"
 
     if [ $? -eq 0 ]; then
         echo "#########################################################################"
-        echo "Container subiu com sucesso"
+        echo "Container subiu com sucesso."
         echo "#########################################################################"
     else
         echo "#########################################################################"
-        echo "Algo deu errado, chama o Diego"
+        echo "Algo deu errado, chama o Diego."
         echo "#########################################################################"
     fi
 else
